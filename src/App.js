e327cb09f9c3b05390962c7e151232df9b54a0c2
Ankit Kumar <ankitkumar@ankits-mac.mynetworksettings.com>
2023-10-16 00:04:02 -0400

    develop check

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>ankit Portfolio</h1>
+        <h1>ankit Portfolio develop check</h1>
       </header>
     </div>
   );
