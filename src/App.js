9fb809a97a27b4794ef5ca97bebe64b6a5b039b6
Ankit Kumar <33374891+ankitkr8540@users.noreply.github.com>
2023-10-16 13:32:52 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>ankit Portfolio develop check</h1>
+        <h1>ankit Portfolio develop check from src file</h1>
       </header>
     </div>
   );
